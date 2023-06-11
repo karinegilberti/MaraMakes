1979411193bceecd76c9ff1e7da4787d9ba4bcd6
Karine Gilberti <126530994+karinegilberti@users.noreply.github.com>
2023-06-11 19:07:14 -0300

    Commit inicial
    Subindo os arquivos do meu site

---
+++ sugestoes.js
@@ -0,0 +1,13 @@
+let sugestoes = [
+    "Base",
+    "Máscara de cílios",
+    "Batom",
+    "Blush",
+    "Pinceis",
+    "Paletas de sombra",
+    "Demaquilante",
+    "Iluminador",
+    "Corretivo",
+    "Libtint",
+    "Delineador",
+]
